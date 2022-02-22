cb6085c61d2ba9d8ea446667e7f33023b9a23ad6
PeterChen <44598938+jinher1997@users.noreply.github.com>
2022-02-22 22:39:07 +0800

    Update notion client feature.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 const dotenv = require('dotenv').config();
+const { Client } = require('@notionhq/client')
+
+//Init clinet
+
+const notionclient = new Client(
+    {
+        auth: process.env.NOTION_TOKEN
+    }
+)
+
+const databaseId = process.env.NOTION_DATABASE_ID
+
+
+async function addItem(text) {
+    try {
+        const response = await notionclient.pages.create({
+            parent: { database_id: databaseId },
+            properties: {
+                title: {
+                    title: [
+                        {
+                            "text": {
+                                "content": text
+                            }
+                        }
+                    ]
+                }
+            },
+        })
+        console.log(response)
+        console.log("Success! Entry added.")
+    } catch (error) {
+        console.error(error.body)
+    }
+}
+
+addItem("Yurts in Big Sur, California")
